44b083a21b1f315e81f3dea603ed0a3b4ac1573f
Bemigho <bemighoneyin47@gmail.com>
2025-08-14 23:23:21 +0300

    Add project files.

---
+++ NeuralNetworkVulkan.cpp
@@ -0,0 +1,12 @@
+﻿// NeuralNetworkVulkan.cpp : Defines the entry point for the application.
+//
+
+#include "NeuralNetworkVulkan.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
